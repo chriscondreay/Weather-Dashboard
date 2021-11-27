a507db2e14273b202cacf24b0820cfa8c754df4c
The Instigator <condreaychris@gmail.com>
2021-11-26 18:32:40 -0800

    handling serachHistory func

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = "78c6f3a7fa231275315ddb5c39a6e226";
 const locations = [];
-const history = document.getElementById("history");
-const locationFormEl = document.getElementById("location-search-form");
+const history = [document.getElementById("history");]
+const locationFormSearchEl = document.getElementById("location-search-form");
 const cityEl = document.getElementById("city");
 const locationSearchEl = document.getElementById("searched-location");
 const weatherContainerEl = document.getElementById("current-weather");
@@ -18,3 +18,68 @@ const getWeather = () => {
         });
     });
 }
+
+function submit(e) {
+  e.preventDefault();
+  const city = cityEl.value.trim();
+  getWeather(city);
+  fiveDays(city);
+  saveSearch(city);
+  previousSearch(city);
+};
+
+function saveSearch() {
+  localStorage.setItem("cities", JSON.stringify(locations));
+};
+
+function displayWeather(weather, searchCity) {
+
+  weatherContainerEl.textContent = "";
+  locationSearchEl.textContent = searchCity;
+
+  const temperatureEl = document.createElement("span");
+  temperatureEl.textContent = `Temperature: ${weather.main.temp} °F`;
+  temperatureEl.classList = "list-group-item"
+  weatherContainerEl.appendChild(temperatureEl);
+
+  const humidityEl = document.createElement("span");
+  humidityEl.textContent = `Humidity: ${weather.main.humidity} %`;
+  humidityEl.classList = "list-group-item"
+  weatherContainerEl.appendChild(humidityEl);
+
+  const windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = `Wind Speed: ${weather.wind.speed} MPH`;
+  windSpeedEl.classList = "list-group-item"
+  weatherContainerEl.appendChild(windSpeedEl);
+
+  const weatherIcon = document.createElement("img")
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+  citySearchInputEl.appendChild(weatherIcon);
+
+  let currentDate = document.createElement("span")
+  currentDate.textContent = " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+  citySearchInputEl.appendChild(currentDate);
+
+  const lat = weather.coord.lat;
+  const lon = weather.coord.lon;
+  uvIndex(lat, lon);
+};
+
+function searchHistory() {
+  history.innerHTML = '';
+
+  // Start at end of history array and count down to show the most recent at the top.
+  for (let i = locations.length - 1; i >= 0; i--) {
+    let btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    // `data-search` allows access to city name when click handler is invoked
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+  }
+}
+locationFormSearchEl.addEventListener("submit", submitSearch);
+history.addEventListener("click", historySearch)
